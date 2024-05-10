2a27e0ca8a7c983c10745f1e21e15ea95475a780
Vũ Ngọc Nam <vungocnam2000nd@gmail.com>
2024-05-10 23:02:01 +0700

    Update enum districtCode

--- src/main/java/com/javaweb/enums/districtCode.java
+++ src/main/java/com/javaweb/enums/districtCode.java
@@ -9,7 +9,10 @@ public enum districtCode {
     QUAN_2("Quận 2"),
     QUAN_3("Quận 3"),
     QUAN_4("Quận 4"),
-    QUAN_5("Quận 5");
+    QUAN_5("Quận 5"),
+    QUAN_10("Quận 10"),
+    QUAN_11("Quận 11"),
+    QUAN_TB("Quận Tân Bình");
 
     private final String districtName;
 
